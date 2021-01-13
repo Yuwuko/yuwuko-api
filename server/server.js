378453b75552e69d5a8ff3e498208918fed61ab1
BasketBandit <errorsfound@hotmail.co.jp>
2021-01-13 13:15:57 +0000

    changed port number

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const router = require('./routes');
 const cors = require('cors')
 const app = express();
 
+const pkey = fs.readFileSync('/etc/letsencrypt/live/nooksworkbench.com/privkey.pem', 'utf8');
+const ccert = fs.readFileSync('/etc/letsencrypt/live/nooksworkbench.com/fullchain.pem', 'utf8');
+const credentials = {
+    key: pkey,
+    cert: ccert,
+    requestCert: false
+};
+
 app.use(cors())
 app.use('/api/', router);
 
 // Listen
-app.listen('8080', () => {
-    console.log('Server running on port: 8080.')
+https.createServer(credentials, app).listen(3000, () => {
+    console.log('Server running on port: 3000.')
 });
