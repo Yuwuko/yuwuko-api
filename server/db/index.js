e2bd1eb869edf11c82e31e6e8e525caf732e2b32
BasketBandit <errorsfound@hotmail.co.jp>
2021-01-22 17:54:13 +0000

    fixed stats query to fetch most current data

--- server/db/index.js
+++ server/db/index.js
@@ -13,7 +13,7 @@ let dbyuuko = {};
 
 dbyuuko.stats = () => {
     return new Promise((resolve, reject) => {
-        connection.query('SELECT metrics_discord.guildCount, metrics_discord.messageEvents, metrics_system.uptime FROM metrics_discord INNER JOIN metrics_system ON metrics_discord.shardId = metrics_system.shardId ORDER BY metrics_system.dateInserted DESC LIMIT 1', (err, results) => {
+        connection.query('SELECT metrics_discord.guildCount, metrics_discord.messageEvents, metrics_system.uptime FROM metrics_discord INNER JOIN metrics_system ON metrics_discord.dateInserted = metrics_system.dateInserted ORDER BY metrics_discord.dateInserted DESC LIMIT 1', (err, results) => {
             if(!err) {
                 return resolve(results[0]);
             }
